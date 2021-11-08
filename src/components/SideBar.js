0569d5b26a44271f74870031c264cfc617206be1
Ezaan Ali <ezaan1999.ali@gmail.com>
2021-11-08 23:50:37 +0500

    updated popup

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,11 +4,14 @@ import { BsPersonCircle, BsPatchCheck, BsFiles, BsPeople } from 'react-icons/bs'
 const SideBar = (props) => {
     const [isOpen, setisOpen] = useState(false);
     const [model, setmodel] = useState('1');
+    const [selectedModel, setselectedModel] = useState(0);
+    const [selectedType, setselectedType] = useState(0);
     const userType = localStorage.getItem('userType');
     const userOrFiles = (userType === 'admin') ? "Users" : "Files";
     const adminOrMember = (userType === 'admin') ? "Admin" : `${props.member} Member`;
 
     const [modelType, setmodelType] = useState("Model");
+    const [count, setcount] = useState(0);
 
     const [title1, settitle1] = useState("Regression");
     const [title2, settitle2] = useState("Classification");
@@ -19,22 +22,26 @@ const SideBar = (props) => {
 
     useEffect(
         () => {
-            if (model === 1) {
+            if (model === 1 && count !== 3) {
                 document.getElementById('1').classList.add('border-indigo-600');
                 document.getElementById('2').classList.remove('border-indigo-600');
                 document.getElementById('3').classList.remove('border-indigo-600');
             }
-            else if (model === 2) {
+            else if (model === 2 && count !== 3) {
                 document.getElementById('2').classList.add('border-indigo-600');
                 document.getElementById('1').classList.remove('border-indigo-600');
                 document.getElementById('3').classList.remove('border-indigo-600');
             }
-            else if (model === 3) {
+            else if (model === 3 && count !== 3) {
                 document.getElementById('3').classList.add('border-indigo-600');
                 document.getElementById('1').classList.remove('border-indigo-600');
                 document.getElementById('2').classList.remove('border-indigo-600');
             }
-        }, [model]
+            else if (count === 3) {
+                document.getElementById('modelButtons').classList.add('hidden');
+                document.getElementById('modelFile').classList.remove('hidden');
+            }
+        }, [model, count]
     )
 
     return (
@@ -56,12 +63,12 @@ const SideBar = (props) => {
             <div className="w-72 h-full bgPattern absolute top-0 left-0 z-0"></div>
             {
                 (isOpen) ?
-                <div className="w-screen h-screen bg-gray-700 bg-opacity-40 flex items-center justify-center absolute top-0 left-0 z-50">
+                <div className="w-screen h-screen bg-gray-700 bg-opacity-40 backdrop-filter backdrop-blur-sm flex items-center justify-center absolute top-0 left-0 z-50">
                     <div className="w-3/5 px-5 py-4 bg-white border-2 rounded-xl flex flex-col">
                         <div className="flex flex-row items-center justify-start">
                             <h1 className="text-gray-600 text-xl font-bold">Choose {modelType}</h1>
                         </div>
-                        <div className="mt-4 flex flex-row items-stretch justify-between">
+                        <div id="modelButtons" className="mt-4 flex flex-row items-stretch justify-between">
                             <button id="1" className="w-1/3 min-h-full mx-1 px-3 py-2 border-2 rounded-lg flex flex-col items-start justify-start overflow-y-auto" onClick={() => setmodel(1)}>
                                 <h1 className="text-left text-gray-600 text-lg font-bold">{title1}</h1>
                                 <p className="text-gray-600 text-left">{para1}</p>
@@ -75,13 +82,31 @@ const SideBar = (props) => {
                                 <p className="text-gray-600 text-left">{para3}</p>
                             </button>
                         </div>
+                        <div id="modelFile" className="hidden mt-4">
+                            <input type="file" multiple="false" accept=".csv" />
+                        </div>
                         <div className="h-full mt-3 flex flex-row items-center justify-end">
-                            <button className="ml-2 px-2 py-1 bg-gray-400 text-white rounded-md" onClick={() => setisOpen(false)}>Close</button>
+                            <button className="ml-2 px-2 py-1 bg-gray-400 text-white rounded-md"
+                                onClick={() => {
+                                    setisOpen(false);
+                                    setmodel('1');
+                                    setcount(0);
+                                    setmodelType("Model");
+                                    settitle1("Regression");
+                                    settitle2("Classification");
+                                    settitle3("Clustering");
+                                    setpara1("Takes a group of random variables, thought to be predicting Y, and tries to find a mathematical relationship between them. This relationship is typically in the form of a straight line (linear regression) that best approximates all the individual data points");
+                                    setpara2("Process of categorizing a given setpara of data into classes, It can be performed on both structured or unstructured data. The process starts with predicting the class of given data points. It approximates the mapping function from input variables to discrete output variables");
+                                    setpara3("Involves automatically discovering natural grouping in data. Unlike supervised learning (like predictive modeling), clustering algorithms only interpret the input data and find natural groups or clusters in feature space");
+                                }}
+                            >Cancel</button>
                             <button id="next" className="ml-2 px-2 py-1 bg-indigo-600 text-white rounded-md"
                                 onClick={
-                                    (model === 1) ? 
+                                    (model === 1 && count === 0) ? 
                                     () => {
-                                        setmodelType("Type")
+                                        setcount(1);
+                                        setselectedModel(0);
+                                        setmodelType("Type");
                                         settitle1("Logistic");
                                         settitle2("Linear");
                                         settitle3("Polynomial");
@@ -89,9 +114,11 @@ const SideBar = (props) => {
                                         setpara2("It is the supervised Machine Learning model in which the model finds the best fit linear line between the dependent and independent variables");
                                         setpara3("It is a form of linear regression in which the relationship between the independent variable x and dependent variable y is modelded as an nth degree polynomial");
                                     } :
-                                    (model === 2) ?
+                                    (model === 2 && count === 0) ?
                                     () => {
-                                        setmodelType("Type")
+                                        setcount(1);
+                                        setselectedModel(1);
+                                        setmodelType("Type");
                                         settitle1("Logistic");
                                         settitle2("K-Nearest Neighbour");
                                         settitle3("Support Vector Machine");
@@ -99,15 +126,36 @@ const SideBar = (props) => {
                                         setpara2("It is an algorithm that stores all available cases and classifies new cases based on similar measure");
                                         setpara3("It is the classification algorithm consisting of many decision trees");
                                     } :
+                                    (model === 3 && count === 0) ?
                                     () => {
-                                        setmodelType("Type")
-                                        settitle1("Logistic");
-                                        settitle2("Linear");
-                                        settitle3("Polynomial");
-                                        setpara1("It is the appropriate regression analysis to conduct when the dependent variable is dichotomous (binary).  Like all regression analyses, the logistic regression is a predictive analysis");
-                                        setpara2("It is the supervised Machine Learning model in which the model finds the best fit linear line between the dependent and independent variables");
-                                        setpara3("It is a form of linear regression in which the relationship between the independent variable x and dependent variable y is modelded as an nth degree polynomial");
-                                    }
+                                        setcount(1);
+                                        setselectedModel(2);
+                                        setmodelType("Type");
+                                        settitle1("Hierarical");
+                                        settitle2("Diverse Hiererical");
+                                        settitle3("Agglomerative");
+                                        setpara1("It is the binary classification model in which output variable is assumes to be equal to a linear combination of the input variables, transformed by the logistic function");
+                                        setpara2("It is also known as top-down approach. This algorithm does not require to pre-specify the number of clusters");
+                                        setpara3("A structure that is more informative than the unstructured set of clusters returned by flat clustering. This clustering algorithm does not require us to pre-specify the number of clusters");
+                                    } :
+                                    (model === 1 && count === 1) ?
+                                    () => {
+                                        setcount(3);
+                                        setselectedType(0);
+                                        setmodelType("File");
+                                    } :
+                                    (model === 2 && count === 1) ?
+                                    () => {
+                                        setcount(3);
+                                        setselectedType(1);
+                                        setmodelType("File");
+                                    } :
+                                    (model === 3 && count === 1) ?
+                                    () => {
+                                        setcount(3);
+                                        setselectedType(2);
+                                        setmodelType("File");
+                                    } : null
                                 }
                             >Next</button>
                         </div>
